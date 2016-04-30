b9e8c83875680d28afd475f24da519421ed2f85d
Aaron Smith <aaron.smith@aspect.com>
2016-04-30 18:30:55 -0400

    adding some database functions

--- smithurl/smithurl/database.py
+++ smithurl/smithurl/database.py
@@ -1,10 +1,14 @@
 import pymysql
 
-connection = pymysql.connect(host='localhost',
-                             user='user',
-                             password='passwd',
-                             db='db',
-                             cursorclass=pymysql.cursors.DictCursor)
+connection = pymysql.connect(host='localhost',user='root',password='y0y0M@!',db='urldb')
 
 def collision_detector(short_url):
-    
+    with connection.cursor() as cursor:
+        sql = "SELECT shorturl FROM urls WHERE shorturl = %s"
+        return cursor.execute(sql, (short_url))
+
+def insert_entry(long_url,short_url):
+    with connection.cursor() as cursor:
+        sql = "insert into urls (entry_date,longurl,shorturl) values (now(),%s,%s)"
+        cursor.execute(sql, (long_url,short_url))
+    connection.commit()
--- smithurl/smithurl/views.py
+++ smithurl/smithurl/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from .forms import UrlForm
 from .shorten import shorten_url
+from .database import collision_detector
+from .database import insert_entry
 
 def get_url(request):
     if request.method == 'POST':
@@ -10,6 +12,9 @@ def get_url(request):
         if form.is_valid():
             long_url = request.POST.get("long_url", "")
             short_url = shorten_url()
+            while collision_detector(short_url) >= 1:
+                short_url = shorten_url()
+            insert_entry(long_url,short_url)
             return HttpResponse("Your shortened URL: {}".format(short_url))
 
     else:
