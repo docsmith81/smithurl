baca9f9e66eea7a0cb9433cb103f3f10950306c1
Aaron Smith <aaron.smith@aspect.com>
2016-04-30 18:38:40 -0400

    replacing boto with boto3

--- smithurl/smithurl/database.py
+++ smithurl/smithurl/database.py
@@ -1,4 +1,5 @@
 import pymysql
+import boto.route53
 
 connection = pymysql.connect(host='localhost',user='root',password='y0y0M@!',db='urldb')
 
@@ -6,9 +7,14 @@ def collision_detector(short_url):
     with connection.cursor() as cursor:
         sql = "SELECT shorturl FROM urls WHERE shorturl = %s"
         return cursor.execute(sql, (short_url))
+    connection.close()
 
 def insert_entry(long_url,short_url):
     with connection.cursor() as cursor:
         sql = "insert into urls (entry_date,longurl,shorturl) values (now(),%s,%s)"
         cursor.execute(sql, (long_url,short_url))
     connection.commit()
+    connection.close()
+
+def send_to_route53(short_url):
+    connection = boto.route53.connect_to_region('us-east-1')
